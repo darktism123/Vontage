d3b02f5c34c7214c6d7462f0a057a0d8ec028ac5
darktism123 <darknezzz123@gmail.com>
2024-12-15 01:35:52 +0700

    remove db_back

--- System/routes_Backend/employee_vontage.js
+++ System/routes_Backend/employee_vontage.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db_back'); // ดึงการตั้งค่าฐานข้อมูล
+const db = require('../config/db'); // ดึงการตั้งค่าฐานข้อมูล
 
 // Endpoint ดึงข้อมูลพนักงานทั้งหมด
 
